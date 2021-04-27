fcf17e57ddc0da83609cc72daa4f0ec5237ac76f
Sylvain RUMEU <mioux-git@insertcoins.net>
2021-04-27 11:23:40 +0200

    INT de taille illimité

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CXX               := clang
-CXXFLAGS          := -pedantic-errors -Wall -Wextra -Werror
-LDFLAGS           := -L/usr/lib -lstdc++
+CXXFLAGS          := -Wall -g
+LDFLAGS           := -L/usr/lib -lstdc++ -lgmpxx -lgmp -I/usr/lib/
 BUILD             := ./build
 TARGET            := $(BUILD)/fibo-linux-x86_64
 SRC               := main.cpp
@@ -9,6 +9,7 @@ ARCHIVE_XX        := tar
 ARCHIVE_FLAGS     := -cvJf
 
 CXX_WIN           := i686-w64-mingw32-g++
+LDFLAGS_WIN       := -L/usr/i686-w64-mingw32/sys-root/mingw/lib -lstdc++ -lgmpxx -lgmp -I/usr/i686-w64-mingw32/sys-root/mingw/include
 TARGET_WIN        := $(BUILD)/fibo-windows-x86.exe
 ARCHIVE_WIN       := $(BUILD)/fibo-windows-x86.zip
 ARCHIVE_XX_WIN    := zip
@@ -17,7 +18,7 @@ ARCHIVE_FLAGS_WIN := -9
 all:
 	-@mkdir -p $(BUILD)
 	-@$(CXX) $(LDFLAGS) $(CXXFLAGS) $(SRC) -o $(TARGET)
-	-@$(CXX_WIN) $(LDFLAGS) $(CXXFLAGS) $(SRC) -o $(TARGET_WIN)
+	-@$(CXX_WIN) $(CXXFLAGS) $(SRC) -o $(TARGET_WIN) $(LDFLAGS_WIN)
 	-@$(ARCHIVE_XX) $(ARCHIVE_FLAGS) $(ARCHIVE) $(TARGET)
 	-@$(ARCHIVE_XX_WIN) $(ARCHIVE_FLAGS_WIN) $(ARCHIVE_WIN) $(TARGET)
 
--- main.cpp
+++ main.cpp
@@ -2,71 +2,16 @@
 #include <stdlib.h>
 #include <ctime>
 #include <ratio>
-#include <chrono>
 #include <climits>
 #include <string.h>
 #include <iomanip>
+#include <gmp.h>
+#include <gmpxx.h>
 
 using namespace std;
-using namespace std::chrono;
 
 unsigned long long *valeurs_recurs = NULL;
 bool do_table = false;
-
-/*
- * Explications :
- * Cet algorithme est récursif (il s'appelle lui même)
- * Il est simple à coder mais plus gourmand en ressources
- * Dans mon tweet, j'avais mis des types « int » (qui signigie « entier relatif (peut être négatif) »)
- * Dans le code j'ai mis «unsigned long long » (qui signifie « entier plus gand que int, positif) »)
- * C'est pour pouvoir profiter des capacités des processeurs actuels qui gèrent des nombres beaucoup plus grand
- * Le « else » a disparu parce qu'il ne servait à rien (et j'ai mis en forme au passsage)
- * Les positions restent en « int » car on ne peut pas calculer le nombre de fibonacci pour le plus grand « int » dans un « long long »
- */
-
-unsigned long long fibo_recurs(unsigned int pos)
-{
-    static bool overflow = false;
-
-    if (valeurs_recurs == NULL)
-    {
-        /* +1 car on veut l'élément de position pos en partant de 0.
-         * Or si un tableau a une longueur de pos, il contient exactement pos élément DONT le 0
-         */
-        valeurs_recurs = new unsigned long long [pos + 1]; 
-        for (unsigned int i = 0; i <= pos; ++i)
-        {
-            valeurs_recurs[i] = (i < 2 ? 1 : 0);
-        }
-    }
-
-    if (valeurs_recurs[pos] != 0)
-    {
-        return valeurs_recurs[pos];
-    }
-
-    unsigned long long val_pos_1 = valeurs_recurs[pos - 1];
-    if (val_pos_1 == 0)
-    {
-        val_pos_1 = fibo_recurs(pos - 1);
-    }
-
-    unsigned long long val_pos_2 = valeurs_recurs[pos - 2];
-    /*
-     * Pas besoin de vérifier que la valeur existe dans le tableau, elle y est forcément
-     * car calculée pour trouver val_pos_1
-     */
-
-    valeurs_recurs[pos] = val_pos_1 + val_pos_2;
-
-    if (overflow == false && valeurs_recurs[pos] < valeurs_recurs[pos - 1])
-    {
-        cerr << "Dépassement de capacité, on ne peut calculer plus loin que le terme " << pos - 1 << "." << endl;
-        overflow = true;
-    }
-
-    return val_pos_1 + val_pos_2;
-}
 
 /*
  * Explications :
@@ -76,55 +21,38 @@ unsigned long long fibo_recurs(unsigned int pos)
  * val_pos est la valeur recherchée
  */
 
-unsigned long long fibo_iter(unsigned int pos)
+void fibo_iter()
 {
-    bool overflow = false;
-
-    if (pos < 2)
-    {
-        return 1;
-    }
-
-    unsigned long long val_pos_2 = 1;
-    unsigned long long val_pos_1 = 1;
-    unsigned long long val_pos;
+    mpz_class i = 0;
+    mpz_class val_pos_2 = 1;
+    mpz_class val_pos_1 = 1;
+    mpz_class val_pos;
 
     size_t headerWidth[3] = {
-        string("Position").size(),
-        string("Valeur              ").size(),
-        string("Phi             ").size()
+        string("Position     ").size(),
+        string("Valeur                                ").size()
     };
 
     if (do_table == true)
     {
-        cout << "Position | Valeur               | Phi             " << endl
-             << "-------------------------------------------------" << endl
-             << "0        | 1                    |                1" << endl
-             << "1        | 1                    |                1" << endl;
+        cout << "Position      | Valeur                                " << endl
+             << "------------------------------------------------------" << endl
+             << "0             | 1                                     " << endl
+             << "1             | 1                                     " << endl;
     }
 
-    for (unsigned int i = 2; i <= pos; ++i)
+    while (true)
     {
         val_pos = val_pos_1 + val_pos_2;
         val_pos_2 = val_pos_1;
         val_pos_1 = val_pos;
 
-        if (overflow == false && val_pos_1 < val_pos_2)
-        {
-            cerr << "Dépassement de capacité, on ne peut calculer plus loin que le terme " << i - 1 << "." << endl;
-            overflow = true;
-        }
+        cout << left << setw(headerWidth[0]) << i << flush;
+        cout << " | ";
+        cout << left << setw(headerWidth[1]) << val_pos << endl;
 
-        if (do_table == true)
-        {
-            cout << left << setw(headerWidth[0]) << i << flush;
-            cout << " | ";
-            cout << left << setw(headerWidth[1]) << val_pos << flush;
-            cout << " | ";
-            cout << right << setw(headerWidth[2]) << ((val_pos_1 * 1.0) / (val_pos_2 * 1.0)) << endl;
-        }
+        ++i;
     }
-    return val_pos;
 }
 
 /*
@@ -133,63 +61,9 @@ unsigned long long fibo_iter(unsigned int pos)
  * À part si vous êtes un développeur, ce n'est pas la partie qui va vous intéresser
  */
 
-int main(int argc, char *argv[])
+int main()
 {
-    unsigned int pos;
-    unsigned long long valeur;
-    unsigned long long valeur_1;
-
-    if (argc > 1 && strcmp(argv[1], "--table") == 0)
-    {
-        do_table = true;
-    }
-
-    if (argc > 1 && strcmp(argv[1], "--table") == 0)
-    {
-        do_table = true;
-    }
-
-    cout.precision(numeric_limits<double>::digits10);
-
-    cout << "Quelle position souhaitez vous calculer ?" << endl << "Rappel, les positions sont calculées à partir de 0." << endl << "Position: ";
-    cin >> pos;
-
-    if (do_table == false)
-    {
-        cout << "Calcul en itératif : " << flush;
-    }
-    high_resolution_clock::time_point start = high_resolution_clock::now();
-    valeur = fibo_iter(pos);
-    duration<double, std::milli> time_span = high_resolution_clock::now() - start;
-    if (do_table == true)
-    {
-        cout << "Calcul en itératif : ";
-    }
-    cout << time_span.count() << "ms" << endl;
-    cout << "Valeur calculée : fibo_iter(" << pos << ") = " << valeur << endl;
-    
-    if (do_table == false)
-    {
-        cout << "Calcul en récursif : " << flush;
-        start = high_resolution_clock::now();
-        valeur = fibo_recurs(pos);
-        time_span = high_resolution_clock::now() - start;
-        cout << time_span.count() << "ms" << endl;
-        cout << "Valeur calculée : fibo_recurs(" << pos << ") = " << valeur << endl;
-    
-
-        cout << "Approximation de φ : " << flush;
-        if (pos > 0)
-        {
-            valeur_1 = fibo_iter(pos - 1);
-            double phi = (valeur * 1.0) / (valeur_1 * 1.0);
-            cout << phi << endl;
-        }
-        else
-        {
-            cout << 1 << endl;
-        }
-    }
+    fibo_iter();
 
     return EXIT_SUCCESS;
 }
